fed60bbf592569f21e8de02819343a8fe96d48d5
J2DCB <junhyeok1209@outlook.kr>
2025-05-10 20:26:32 +0900

    250510 Ui 관련 적용

--- main.cpp
+++ main.cpp
@@ -1,109 +1,70 @@
 #include <gtk/gtk.h>
 #include <locale.h>
-#include <pango/pango.h>
-#include <glib.h>
-
-
-static void ComponentHandler ( GtkWidget* gtk_window_ , GtkWidget* gtk_component ) {
-	GError* error = NULL;
-	
-	
-	gtk_window_set_child ( GTK_WINDOW ( gtk_window_ ) , GTK_WIDGET ( gtk_component ) );
-
-
-}
-
-
-
-
-
-	
-static void ButtonShow(GtkWidget* gtk_window_, GtkWidget* gtk_button) {  
-   gtk_window_set_child(GTK_WINDOW(gtk_window_), GTK_WIDGET(gtk_button));  
-}
-
 
 static void ButtonClick ( GtkButton* button , gpointer user_data ) {
 	gtk_button_set_label ( button , "Clicked" );
 }
 
 
+static gboolean on_window_close ( GtkWindow* window , gpointer user_data ) {
 
-static void LabelShow (GtkWidget* gtk_window_, const char* string_) {
 
-	GtkWidget* label = gtk_label_new (string_);
-
-	gtk_window_set_child ( GTK_WINDOW ( gtk_window_ ) , label );
-
+	g_application_quit ( G_APPLICATION ( user_data ) );
+	return GDK_EVENT_STOP;  
 }
 
-
-
-
-static void on_activate(GtkApplication* app, gpointer user_data) {
-    GtkWidget* window = gtk_application_window_new(app);
-
-	GError* error = NULL;
-
-	const char* local_string = "Jejunu";
-
-	gchar* utf8_string = g_locale_to_utf8 ( local_string , -1 , NULL , NULL , &error );
-
-	if (!utf8_string) {
-		g_print ( "변환 실패: %s\n" , error->message );
-		g_error_free ( error );
-		return;
-	}
-    gtk_window_set_title(GTK_WINDOW(window), utf8_string );
-    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
-
-
-	
-	GtkWidget* label = gtk_label_new( utf8_string );
-	gtk_window_set_child ( GTK_WINDOW ( window ) , label );
-
-	LabelShow (window, "helo");
-
-	GtkWidget* button = gtk_button_new_with_label ( "Activate" );
-	ButtonShow ( window , button );
-	g_signal_connect( button , "clicked" , G_CALLBACK ( ButtonClick ) , NULL );
-
-    GtkCssProvider* provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_string(provider,
-        "* { font-family: 'Malgun Gothic'; font-size: 14pt; }");
-
-    GdkDisplay* display = gdk_display_get_default();
-    gtk_style_context_add_provider_for_display(display,
-        GTK_STYLE_PROVIDER(provider),
-        GTK_STYLE_PROVIDER_PRIORITY_USER);
-
-
-    gtk_window_present(GTK_WINDOW(window));
-}
-
-
-
-// Encoding Setting
-void localeChange() {
-
+void localeChange ( ) {
 	setlocale ( LC_ALL , "ko_KR.UTF-8" );
 	printf ( "Current Locale: %s\n" , setlocale ( LC_ALL , NULL ) );
-
 }
-int main(int argc, char** argv)
-{
 
+static void on_activate ( GtkApplication* app , gpointer user_data ) {
+	GtkBuilder* builder;
+	GObject* window , * button, *button2;
+	GError* error = NULL;
+
+	builder = gtk_builder_new ( );
+	if (!gtk_builder_add_from_file ( builder , "interface.ui" , &error )) {
+		g_printerr ( "Error loading UI file: %s\n" , error->message );
+		g_clear_error ( &error );
+		return;
+	}
+
+	window = gtk_builder_get_object ( builder , "main_window" );
+	button = gtk_builder_get_object ( builder , "main_button" );
+	button2 = gtk_builder_get_object ( builder , "exit_button" );
+
+
+	gtk_window_set_application ( GTK_WINDOW ( window ) , app );
+
+
+	g_signal_connect ( button , "clicked" , G_CALLBACK ( ButtonClick ) , NULL );
+	g_signal_connect ( button2 , "clicked" , G_CALLBACK ( on_window_close ) , window );
+
+	// CSS 설정
+	GtkCssProvider* provider = gtk_css_provider_new ( );
+	gtk_css_provider_load_from_string ( provider ,
+		"* { font-family: 'Malgun Gothic'; font-size: 14pt; }" );
+
+	GdkDisplay* display = gdk_display_get_default ( );
+	gtk_style_context_add_provider_for_display ( display ,
+		GTK_STYLE_PROVIDER ( provider ) ,
+		GTK_STYLE_PROVIDER_PRIORITY_USER );
+
+	gtk_window_present ( GTK_WINDOW ( window ) );
+	g_object_unref ( builder );
+}
+
+int main ( int argc , char** argv ) {
 	localeChange ( );
+
 	GtkApplication* app;
 	int status;
 
-	PangoFontDescription* font_desc = pango_font_description_from_string("Malgun Gothic 12");
-	pango_font_description_free(font_desc);
+	app = gtk_application_new ( "com.gtk.GtkApp" , G_APPLICATION_FLAGS_NONE );
+	g_signal_connect ( app , "activate" , G_CALLBACK ( on_activate ) , NULL );
 
-	app = gtk_application_new("com.gtk.GtkApp", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-
-	status = g_application_run(G_APPLICATION(app), argc, argv);
-	g_object_unref(app);
+	status = g_application_run ( G_APPLICATION ( app ) , argc , argv );
+	g_object_unref ( app );
 	return status;
 }
