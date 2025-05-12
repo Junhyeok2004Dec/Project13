d4ea4c14d105edac3f1709282a7c0c474456eee4
J2DCB <junhyeok1209@outlook.kr>
2025-05-12 17:59:07 +0900

    250512 Button 관련 공부 #2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <gtk/gtk.h>
 #include <locale.h>
 
-static void ButtonClick ( GtkButton* button , gpointer user_data ) {
-	gtk_button_set_label ( button , "Clicked" );
+#include "TextEnumrate.h"
+
+
+GtkBuilder* builder;
+GObject* window , * car_connection_button , * car_register_button, *exit_button;
+GError* error;
+GObject* title_label , * label_1;
+
+constexpr unsigned int HashCode ( const char* str )
+{
+	return str[ 0 ] ? static_cast<unsigned int>(str[ 0 ]) + 0xEDB8832Full * HashCode ( str + 1 ) : 8603;
 }
+
+static void ButtonClick ( GtkButton* button , gpointer user_data ) {
+	gtk_label_set_label ( GTK_LABEL ( label_1 ) , "차량접속" );
+
+
+	const char* name = gtk_buildable_get_buildable_id( GTK_BUILDABLE ( button ) );
+
+	printf ( "%d", g_utf8_validate ( "차량접속" , -1 , NULL ) );
+	printf ( "\t%s" , name );
+	switch (HashCode(name))
+	{
+	case HashCode("car_connection_button"):
+		gtk_label_set_label ( GTK_LABEL ( label_1 ) , "차량접속" );
+		break;
+	case HashCode("car_register_button") :
+		gtk_label_set_label ( GTK_LABEL ( label_1 ) , "차량등록" );
+		break;
+	default:
+		gtk_label_set_label ( GTK_LABEL ( label_1 ) , "test" );
+		break;
+	}
+
+
+}
+
+
+
 
 
 static gboolean on_window_close ( GtkButton* button, gpointer user_data ) {
@@ -18,10 +54,8 @@ void localeChange ( ) {
 	printf ( "Current Locale: %s\n" , setlocale ( LC_ALL , NULL ) );
 }
 
-static void on_activate ( GtkApplication* app , gpointer user_data ) {
-	GtkBuilder* builder;
-	GObject* window , * button, *button2;
-	GError* error = NULL;
+static void init_components ( GtkApplication* app , gpointer user_data ) {
+	// Initialize components here
 
 	builder = gtk_builder_new ( );
 	if (!gtk_builder_add_from_file ( builder , "interface.ui" , &error )) {
@@ -29,17 +63,31 @@ static void on_activate ( GtkApplication* app , gpointer user_data ) {
 		g_clear_error ( &error );
 		return;
 	}
-
 	window = gtk_builder_get_object ( builder , "main_window" );
-	button = gtk_builder_get_object ( builder , "main_button" );
-	button2 = gtk_builder_get_object ( builder , "exit_button" );
+	car_connection_button = gtk_builder_get_object ( builder , "car_connection_button" );
+	car_register_button = gtk_builder_get_object ( builder , "car_register_button" );
+	title_label = gtk_builder_get_object ( builder , "title_label" );
+	label_1 = gtk_builder_get_object ( builder , "text_display_1_label" );
 
 
+}
+
+
+static void on_window_destroy ( GtkWidget* widget , gpointer user_data ) {
+	g_application_quit ( G_APPLICATION ( user_data ) );
+}
+
+
+static void on_activate ( GtkApplication* app , gpointer user_data ) {
+
+	error = NULL;
+	init_components ( app , user_data );
 	gtk_window_set_application ( GTK_WINDOW ( window ) , app );
 
 
-	g_signal_connect ( button , "clicked" , G_CALLBACK ( ButtonClick ) , NULL );
-	g_signal_connect ( button2 , "clicked" , G_CALLBACK ( on_window_close ) , NULL,  window );
+	g_signal_connect ( car_connection_button , "clicked" , G_CALLBACK ( ButtonClick ) , NULL );
+	g_signal_connect ( car_register_button , "clicked" , G_CALLBACK ( ButtonClick ) , NULL );
+	g_signal_connect ( exit_button , "clicked" , G_CALLBACK ( on_window_close ) , NULL );
 
 	// CSS 설정
 	GtkCssProvider* provider = gtk_css_provider_new ( );
